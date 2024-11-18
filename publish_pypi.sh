8cf867146f8edd4be479cd6daf2ac67cd4b67a50
Marcus Rickert <marcus.rickert@web.de>
2024-11-18 23:10:17 +0100

    Configure default upload to PYPI test using token

--- publish_pypi.sh
+++ publish_pypi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(dirname "$0")
 
-USER=${USER:-marcus.rickert@web.de}
-REPOSITORY_URL=${REPOSITORY_URL:-testpypi}
+USER=${USER:-__token__}
+REPOSITORY_URL=${REPOSITORY_URL:-https://test.pypi.org/legacy/}
 FILE=$(ls dist/*.tar.gz)
 echo "Uploading $FILE as $USER to $REPOSITORY_URL..."
 pip install twine
